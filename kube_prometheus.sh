7200ac42cd2338e8204e6e58ad60ca3a739bb2e5
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2022-06-04 00:20:21 -0300

    Update kube_prometheus.sh

--- kube_prometheus.sh
+++ kube_prometheus.sh
@@ -3,21 +3,16 @@
 readonly INSTALL_DIR=kube-prometheus-instalation
 readonly RELEASE_VERSION=0.9
 
-# Limpando diretorio
-rm -r ${INSTALL_DIR} &>/dev/null
-rm -r ${INSTALL_DIR}/manifests/ &>/dev/null
-
-echo "Download dos manifestos kube-prometheus - release ${RELEASE_VERSION}"
-mkdir ${INSTALL_DIR}
-curl \
-  -L https://github.com/prometheus-operator/kube-prometheus/archive/refs/heads/release-${RELEASE_VERSION}.zip \
-  -o ${INSTALL_DIR}/release.zip
+echo "Clonando manifestos kube-prometheus - release ${RELEASE_VERSION}"
+curl -LO https://github.com/prometheus-operator/kube-prometheus/archive/refs/heads/release-${RELEASE_VERSION}.zip
 
 sleep 2
 echo "Descompactando arquivos"
-unzip -q ${INSTALL_DIR}/release.zip
+rm -r ${INSTALL_DIR} &>/dev/null
+mkdir ${INSTALL_DIR}
+unzip -q release-${RELEASE_VERSION}.zip
 mv --force kube-prometheus-release-${RELEASE_VERSION}/manifests ${INSTALL_DIR}
-rm -r ${INSTALL_DIR}/release.zip
+rm -r release-${RELEASE_VERSION}.zip
 rm -r kube-prometheus-release-${RELEASE_VERSION}
 
 sleep 5
@@ -35,4 +30,4 @@ echo "{\"spec\": {\"ports\": [{\"name\": \"http\" ,\"port\": 3000,\"targetPort\"
 kubectl patch \
  --namespace monitoring \
  service grafana \
- -p '{"spec": {"ports": [{"name": "http" ,"port": 3000,"targetPort": "http","nodePort":30500}],"type": "NodePort"}}' 
+  -p '{"spec": {"ports": [{"name": "http" ,"port": 3000,"targetPort": "http","nodePort":30500}],"type": "NodePort"}}' 
