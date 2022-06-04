77c5b8c61326c4585559cc182446d4a59be9adbf
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2022-06-04 00:09:14 -0300

    Update kube_prometheus.sh

--- kube_prometheus.sh
+++ kube_prometheus.sh
@@ -4,10 +4,10 @@ readonly INSTALL_DIR=kube-prometheus-instalation
 readonly RELEASE_VERSION=0.9
 
 # Limpando diretorio
-rm -r $INSTALL_DIR &>/dev/null
+rm -r ${INSTALL_DIR} &>/dev/null
 rm -r ${INSTALL_DIR}/manifests/ &>/dev/null
 
-echo "Download dos manifestos kube-prometheus - release $RELEASE_VERSION"
+echo "Download dos manifestos kube-prometheus - release ${RELEASE_VERSION}"
 mkdir ${INSTALL_DIR}
 curl \
   -L https://github.com/prometheus-operator/kube-prometheus/archive/refs/heads/release-${RELEASE_VERSION}.zip \
@@ -30,8 +30,9 @@ kubectl apply -f ${INSTALL_DIR}/manifests/ &>/dev/null
 
 sleep 5
 echo "Realizando patch em /manifests/grafana-service.yaml"
+echo "grafana-service.yaml exposto via NodePort 30500"
 echo "{\"spec\": {\"ports\": [{\"name\": \"http\" ,\"port\": 3000,\"targetPort\": \"http\",\"nodePort\":30500}],\"type\": \"NodePort\"}}";
 kubectl patch \
  --namespace monitoring \
  service grafana \
-  -p '{"spec": {"ports": [{"name": "http" ,"port": 3000,"targetPort": "http","nodePort":30500}],"type": "NodePort"}}' 
+ -p '{"spec": {"ports": [{"name": "http" ,"port": 3000,"targetPort": "http","nodePort":30500}],"type": "NodePort"}}' 
