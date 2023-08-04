7cd56faefa890057c81b023e6f8803424b545efe
RafaelClaumann <rafael_claumann@hotmail.com>
2023-08-04 20:50:30 -0300

    ajuste logs server.py

--- validating-admission-webhook/app/server.py
+++ validating-admission-webhook/app/server.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def validating_endpoint():
     req = request.get_json()['request']
 
-    app.logger.info('uid: %s, namespace: %s, operation: %s', req['uid'], req['namespace'], req['operation'])
-    app.logger.info('userInfo: %s, options: %s', req['userInfo'], req['options'])
-    app.logger.info('kind: %s, requestKind: %s', req['kind'], req['requestKind'])
-    
+    app.logger.info('uid: %s, namespace: %s, name: %s', req['uid'], req['namespace'], req['name'])
+    app.logger.info('userInfo: %s', req['userInfo'])
+    app.logger.debug('object: %s', req['object'])
+
     return jsonify(
         {
             'apiVersion': 'admission.k8s.io/v1',
